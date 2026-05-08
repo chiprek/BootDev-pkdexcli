87246fe913b833974b0ca8003a88397ea38132d3
chiprek <chiprek.cs@gmail.com>
2026-05-07 19:19:56 -0500

    implemented a method to repeat back the first word of the input.

--- repl.go
+++ repl.go
@@ -1,9 +1,30 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	sanitized := strings.ToLower(text)
 	words := strings.Fields(sanitized)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
 }
