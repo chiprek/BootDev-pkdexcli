fcd013e42adc04b2de640e4d5c4e8fdcab10ea7d
chiprek <chiprek.cs@gmail.com>
2026-05-13 19:28:17 -0500

    updated type locations

--- internal/pokeapi/type_locations.go
+++ internal/pokeapi/type_locations.go
@@ -1,11 +1,47 @@
 package pokeapi
 
 type RespShallowLocations struct {
-	Count    int     `json:"count"`
-	Next     *string `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
+	Count    int        `json:"count"`
+	Next     *string    `json:"next"`
+	Previous *string    `json:"previous"`
+	Results  []Location `json:"results"`
+}
+
+type Location struct {
+	Name              string       `json:"name"`
+	PokemonEncounters []Encounters `json:"pokemon_encounters"`
+	URL               string       `json:"url"`
+}
+
+type Encounters struct {
+	Pokemon Pokemon `json:"pokemon"`
+}
+
+type Pokemon struct {
+	Name           string        `json:"name"`
+	BaseExperience int           `json:"base_experience"`
+	Height         int           `json:"height"`
+	Weight         int           `json:"weight"`
+	Stats          []PokemonStat `json:"stats"`
+	Types          []PokemonType `json:"types"`
+}
+
+type PokemonStat struct {
+	BaseStat int      `json:"base_stat"`
+	Effort   int      `json:"effort"`
+	Stat     StatInfo `json:"stat"`
+}
+
+type StatInfo struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type PokemonType struct {
+	Type TypeInfo `json:"type"`
+}
+
+type TypeInfo struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
